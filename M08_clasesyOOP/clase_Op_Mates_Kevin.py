076a1a0124bf18cb26716a08aadfa59d8fe3c072
Kevin Elem Jordy Vilchez Lopez <114453705+KvilchezL@users.noreply.github.com>
2023-10-30 23:21:38 -0500

    Homework09_Desarrollado_porfin

--- M08_clasesyOOP/clase_Op_Mates_Kevin.py
+++ M08_clasesyOOP/clase_Op_Mates_Kevin.py
@@ -1,8 +1,8 @@
 class Op_Mates:
-    def __init__(self, lista,num):
+    def __init__(self, lista):
         if not isinstance(lista,list):
             #self.lista=[]
-            raise ValueError('Los datos ingresados son inválidos, debe ingresar una lista')
+            raise ValueError('Los datos ingresados son inválidos, debe ingresar una lista de numeros enteros')
         
         for num  in lista:
             if not isinstance(num,int):
@@ -12,19 +12,45 @@ class Op_Mates:
         self.lista = lista
 
     def hallar_primo(self):
+        resultados = []
+        mensajes = []
         for i in self.lista:
-            if self.__hallar_primo(i):
-                print('El elemento', i, 'SI es un numero primo')
+            es_primo = self.__hallar_primo(i)
+            resultados.append(es_primo)  # Agregar True o False a la lista
+            if es_primo:
+                mensajes.append(f'El elemento {i} SI es un número primo')
             else:
-                print('El elemento', i, 'NO es un numero primo')
+                mensajes.append(f'El elemento {i} NO es un número primo')
+        return resultados, mensajes
     
     def conv_grados(self, medidai, medidaf):
-        for i in self.lista:
-            print(i, '°', medidai, 'son', self.__conv_grados(i, medidai, medidaf), '°', medidaf)
+        
+        try:
+            if not (medidai in ('c', 'C', 'f', 'F', 'k', 'K')) or not (medidaf in ('c', 'C', 'f', 'F', 'k', 'K')):
+                raise ValueError("No se puede convertir!, ingrese 'c' o 'C' para celsius, 'f' o 'F' para farenheit y 'k' o 'K' para kelvin.")
+                
+            grados = []
+            mensajes = []
+            for i in self.lista:
+                grado=self.__conv_grados(i, medidai, medidaf)
+                grados.append(grado)
+                mensaje = f"{i} ° {medidai} son {grado} ° {medidaf}"
+                mensajes.append(mensaje)
+                #print(i, '°', medidai, 'son', self.__conv_grados(i, medidai, medidaf), '°', medidaf)
+            return grados, mensajes
+        except ValueError as e:
+            print(e)  # Imprimir solo el mensaje personalizado de la excepción
 
     def factorial(self):
+        factoriales = []
+        mensajes = []
         for i in self.lista:
-            print('El factorial de', i, 'es', self.__factorial(i))
+            factorial = self.__factorial(i)
+            factoriales.append(factorial)
+            mensaje = f'El factorial de {i} es {factorial}'
+            mensajes.append(mensaje)
+            #print('El factorial de', i, 'es', self.__factorial(i))
+        return factoriales, mensajes
 
     def __hallar_primo(self, nro):
         primo=True
@@ -53,6 +79,7 @@ class Op_Mates:
         return mas_repetido, rep
 
     def __conv_grados(self,i, medidai, medidaf):
+        
                 
         if (medidai == 'c' or medidai == 'C') and (medidaf == 'f' or medidaf == 'F'):
             GC = (i * 9/5) + 32
@@ -78,9 +105,12 @@ class Op_Mates:
             return 'Inválido!, ingrese solo números enteros'
         
         
-# Excepcion
+# Excepciones
+
 if __name__ == "__main__":
     try:
         Mate1 = Op_Mates([1, 2, 3, 4, 5])  # Esto generará un error y mostrará un mensaje de excepción
-    except Exception as e:
-        print(f"Se produjo un error: {e}")
+        
+    except TypeError as e:
+        print("Se produjo un error: {e}")
+
